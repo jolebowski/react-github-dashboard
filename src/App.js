1ed4b570b52dec78ff527a184bb27f94cd6c7eb0
jolebowski <jordan.moreira@epitech.eu>
2021-08-30 13:27:30 +0200

    show Username

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <>
       <div className='flex flex-col h-screen'>
-        <header className='py-5 bg-blue-400 text-white text-center'>
+        <header className='py-5 bg-blue-400 text-white text-center text-3xl'>
           Github Dashboard Sample
         </header>
         <SearchField />
--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -2,32 +2,50 @@ import React, { useState } from 'react';
 
 export default function SearchField() {
   const [username, SetUsername] = useState('');
+  const [data, setData] = useState([]);
+  const [repos, setRepos] = useState([]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const profile = await fetch(
       `https://api.github.com/users/${username}`
     ).then((res) => res.json());
 
-    console.log(profile, 'proprofile');
+    const repositories = await fetch(profile.repos_url).then((res) =>
+      res.json()
+    );
+
+    if (profile) {
+      setData(profile);
+      setRepos(repositories);
+    }
+    SetUsername('');
   };
   return (
-    <div className='flex items-center justify-center p-8'>
-      <div className='flex border-b-2 border-gray-200'>
-        <input
-          type='text'
-          className='px-4 py-2 w-80'
-          value={username}
-          onChange={(e) => SetUsername(e.target.value)}
-          placeholder='Username'
-        />
-        <button
-          type='submit'
-          onClick={handleSubmit}
-          className='px-4 text-white bg-pink-500 border-l uppercase'
-        >
-          Search
-        </button>
+    <>
+      <div className='flex items-center justify-center p-8'>
+        <div className='flex border-b-2 border-gray-200'>
+          <input
+            type='text'
+            className='px-4 py-2 w-80'
+            value={username}
+            onChange={(e) => SetUsername(e.target.value)}
+            placeholder='Username'
+          />
+          <button
+            type='submit'
+            onClick={handleSubmit}
+            className='px-4 text-white bg-pink-500 border-l uppercase'
+          >
+            Search
+          </button>
+        </div>
       </div>
-    </div>
+      <div className='flex items-center justify-center'>
+        <div className='flex mt-5 bg-gray-300 px-2 py-1 rounded'>
+          <p className='ml-3'>{data.login}</p>
+        </div>
+      </div>
+    </>
   );
 }
