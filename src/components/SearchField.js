08a2e5aed4107d05e3ce7cb94055824dbc832be0
jolebowski <jordan.moreira@epitech.eu>
2021-08-30 13:42:25 +0200

    Show user's avatar

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 export default function SearchField() {
   const [username, SetUsername] = useState('');
   const [data, setData] = useState([]);
-  const [repos, setRepos] = useState([]);
+  //const [repos, setRepos] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,10 +17,11 @@ export default function SearchField() {
 
     if (profile) {
       setData(profile);
-      setRepos(repositories);
+      //setRepos(repositories);
     }
     SetUsername('');
   };
+  console.log(data, 'data');
   return (
     <>
       <div className='flex items-center justify-center p-8'>
@@ -43,6 +44,11 @@ export default function SearchField() {
       </div>
       <div className='flex items-center justify-center'>
         <div className='flex mt-5 bg-gray-300 px-2 py-1 rounded'>
+          <img
+            alt='avatar'
+            className='w-7 rounded-full border-2 border-gray-300'
+            src={data.avatar_url}
+          />
           <p className='ml-3'>{data.login}</p>
         </div>
       </div>
