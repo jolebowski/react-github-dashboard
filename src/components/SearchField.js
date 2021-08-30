1feda65cadf0df551b236940a89d17b94717c8a5
jolebowski <jordan.moreira@epitech.eu>
2021-08-30 12:51:33 +0200

    logic of research

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SearchField() {
+  const [username, SetUsername] = useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const profile = await fetch(
+      `https://api.github.com/users/${username}`
+    ).then((res) => res.json());
+
+    console.log(profile, 'proprofile');
+  };
   return (
     <div className='flex items-center justify-center p-8'>
       <div className='flex border-b-2 border-gray-200'>
-        <input type='text' className='px-4 py-2 w-80' placeholder='Username' />
-        <button className='px-4 text-white bg-pink-500 border-l uppercase'>
+        <input
+          type='text'
+          className='px-4 py-2 w-80'
+          value={username}
+          onChange={(e) => SetUsername(e.target.value)}
+          placeholder='Username'
+        />
+        <button
+          type='submit'
+          onClick={handleSubmit}
+          className='px-4 text-white bg-pink-500 border-l uppercase'
+        >
           Search
         </button>
       </div>
